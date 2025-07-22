8094115bed51195808e5c6215d16966994a44708
Boshen <boshenc@gmail.com>
2025-07-22 16:38:07 +0800

    feat(tui): initialize vite tui (#9)
    * feat(tui): initialize vite tui
    
    Project setup and modifier after from running `cargo generate ratatui/templates`
    and then select `component`.
    
    * feat(tui): initialize vite tui
    
    Project setup and modifier after from running `cargo generate ratatui/templates`
    and then select `component`.
    
    * render components
    
    * add pseudo_term
    
    * replace `top` command to task pane
    
    * select tasks
    
    * spawn tasks and navigate
    
    * Move deps into workspace
    
    * Add click event handle
    
    * disallow multiple crate versions
    
    ---------
    
    Co-authored-by: LongYinan <lynweklm@gmail.com>

---
+++ crates/vite_tui/src/action.rs
@@ -0,0 +1,16 @@
+#[expect(unused)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Action {
+    Tick,
+    Render,
+    Resize(u16, u16),
+    Suspend,
+    Resume,
+    Quit,
+    ClearScreen,
+    Error(String),
+    Task { task: String, bytes: Box<[u8]> },
+    Up,
+    Down,
+    SelectTask(usize),
+}
